9165454fe0327d9f39cece6b1e1dcd18960b8a56
Jeong Jae Lee <wjdwo2287@gmail.com>
2026-07-02 20:23:58 +0900

    [level 2] Title: 디펜스 게임, Time: 68.46 ms, Memory: 44.4 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.py
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.py
@@ -1,22 +1,9 @@
-from heapq import heappush, heappop
-
+import heapq as hq
 def solution(n, k, enemy):
-    answer = 0
-    heap = []
-    
-    for i in range(len(enemy)):
-        
-        
-        if n - enemy[i] < 0:
-            if k == 0: break
-            heappush(heap, -enemy[i])
-            n -= heappop(heap)
-            k -= 1
-            n -= enemy[i]
-        
-        else:
-            n -= enemy[i]
-            heappush(heap, -enemy[i])
-        answer += 1
-        
-    return answer
+    q = enemy[:k]
+    hq.heapify(q)
+    for idx in range(k,len(enemy)):
+        n -= hq.heappushpop(q,enemy[idx])
+        if n < 0:
+            return idx
+    return len(enemy)
