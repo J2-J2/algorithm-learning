2755d422ec9c4257b86416a27ab6fd339dad3539
Jeong Jae Lee <wjdwo2287@gmail.com>
2026-07-08 20:34:05 +0900

    [level 2] Title: 우박수열 정적분, Time: 2.24 ms, Memory: 13.2 MB -BaekjoonHub

--- 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.py
+++ 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.py
@@ -1,23 +1,15 @@
-def integral(start, end, values, n):
-    ret = values[start] + values[n+end]
-    for i in range(start+1, n+end):
-        ret += values[i] * 2
-    return ret / 2
-
 def solution(k, ranges):
-    values = [k]
     answer = []
-    n = 0
+    arr = [k]
     while k > 1:
-        if k % 2 == 0: k = k // 2
-        else: k = k * 3 + 1
-        values.append(k)
-        n += 1
-    for start, end in ranges:
-        if start == n + end: answer.append(0.0)
-        elif start > n + end: answer.append(-1.0)
-        elif start >= n: answer.append(0.0)
-        else:
-            if end > n: end = n
-            answer.append(integral(start, end, values, n))
+        if not k % 2: k //= 2
+        else: k = k*3+1
+        arr.append(k)
+    area = [0]
+    for i in range(len(arr)-1):
+        area.append(area[-1]+(arr[i]+arr[i+1])/2)
+    for a, b in ranges:
+        if a >= len(area) or b-1 < -len(area): answer.append(-1)
+        elif area[b-1]-area[a] < 0: answer.append(-1)
+        else: answer.append(area[b-1]-area[a])
     return answer
