723591d93ab8905573382aa824e8e9cb9e72b074
Jeong Jae Lee <wjdwo2287@gmail.com>
2026-07-12 16:39:20 +0900

    [level 2] Title: 지게차와 크레인, Time: 4.70 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.py
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.py
@@ -1,60 +1,48 @@
-import sys
-sys.setrecursionlimit(100000)
-
-def dfs(visited, storage, i, j, target):
-    dy = [-1, 0, 1, 0]
-    dx = [0, 1, 0, -1]
-    temp = []
-    visited[i][j] = 1
-    
-    for k in range(4):
-        ni = i + dy[k]
-        nj = j + dx[k]
-        
-        if 0 <= ni < len(storage) and 0 <= nj < len(storage[0]) and visited[ni][nj] == 0:
-            if storage[ni][nj] == target:
-                temp.append([ni, nj])
-            if storage[ni][nj] == '.':
-                temp += dfs(visited, storage, ni, nj, target)
-    return temp
-    
-
-
+from collections import deque
 def solution(storage, requests):
     answer = 0
-
-    storage = [list(r) for r in storage]
-    
-    find = []
+    edge = set()
+    dx = [0, 0, -1, 1]
+    dy = [1, -1, 0 , 0]
+    matrix = [[""] * (len(storage[0])+2) for _ in range(len(storage)+2)]
     for i in range(len(storage)):
-        for j in range(len(storage[0])):
-            if i in [0, len(storage)-1] or j in [0, len(storage[0])-1]:
-                find.append([i, j])
-    
-    for k in range(len(requests)):
-        target = requests[k]
-        temp = []
-        
-        if len(target) == 1:
-            visited = [[0 for _ in range(len(storage[0]))] for _ in range(len(storage))]
-            
-            for i, j in find:
-                if storage[i][j] == target: temp.append([i, j])
-                elif storage[i][j] == '.' and visited[i][j] == 0: 
-                    
-                    temp += dfs(visited, storage, i, j, target)
-                
-                        
+        for j in range(len(storage[i])):
+            matrix[i+1][j+1] = storage[i][j]
+    for j in range(1, len(storage[0])+1):
+        for i in [1, len(storage)]:
+            edge.add((i,j))
+    for i in range(2, len(storage)):
+        for j in [1, len(storage[0])]:
+            edge.add((i,j))
+    def deleteChar(to_del):
+        for i,j in to_del:
+            edge.remove((i,j))
+            q = deque([(i,j)])
+            while q:
+                x, y = q.popleft()
+                for d in range(len(dx)):
+                    nx = x + dx[d]
+                    ny = y + dy[d]
+                    if 1<=nx<len(storage) and 1<=ny<=len(storage[0]):
+                        if matrix[nx][ny] == " ":
+                            matrix[nx][ny] = ""
+                            q.append((nx,ny))
+                        elif matrix[nx][ny] != "":
+                            edge.add((nx,ny))
+    for request in requests:
+        to_del = set()
+        if len(request) == 1:
+            for i,j in edge:
+                if matrix[i][j] == request:
+                    to_del.add((i,j))
+                    matrix[i][j]=""
+            deleteChar(to_del)
         else:
-            for i in range(len(storage)):
-                for j in range(len(storage[0])):
-                    if storage[i][j] == target[0]:
-                        temp.append([i, j])
-        for i, j in temp:
-            storage[i][j] = '.'
-        
-    for i in range(len(storage)):
-        for j in range(len(storage[0])):
-            if storage[i][j] != '.': answer += 1
-    
-    return answer
+            for i in range(1, len(storage)+1):
+                for j in range(1, len(storage[0])+1):
+                    if matrix[i][j] == request[0]:
+                        matrix[i][j] = " "
+                        if (i,j) in edge:
+                            to_del.add((i,j))
+            deleteChar(to_del)
+    return sum([1 if matrix[i][j] != "" and matrix[i][j] != " " else 0 for i in range(1, len(storage)+1) for j in range(1, len(storage[0])+1)])
