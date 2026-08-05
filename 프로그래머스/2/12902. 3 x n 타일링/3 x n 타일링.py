3fbd1d52b43cefc1c2211841f42f261e0786a705
Jeong Jae Lee <wjdwo2287@gmail.com>
2026-08-05 22:55:40 +0900

    [level 2] Title: 3 x n 타일링, Time: 255.51 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.py
@@ -0,0 +1,15 @@
+def solution(n):
+    answer = 3
+    if n % 2 == 1: return 0
+
+    k = n // 2    
+    temp = [1] * (k+1)
+    temp[1] = 3
+    for i in range(2, k+1):
+        value = 3 * temp[i-1] % 1000000007
+        for j in range(0, i-1):
+            value += 2 * temp[j] % 1000000007
+        temp[i] = value % 1000000007
+
+    print(temp)
+    return temp[-1]  % 1000000007
