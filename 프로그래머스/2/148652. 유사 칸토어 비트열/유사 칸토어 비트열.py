1c2847497b4d3a8227d5d65eb1ed0abb493c7914
Jeong Jae Lee <wjdwo2287@gmail.com>
2026-08-08 18:22:16 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 49.86 ms, Memory: 30.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.py
@@ -0,0 +1,26 @@
+def solution(n, l, r):
+    answer = 0
+    a = ((l-1) // 5) * 5
+    b = ((r-1) // 5 + 1) * 5
+    ls = [[a, b]]
+
+    ret = "1"
+    for i in range(n-1):
+        a = a // 5
+        b = b // 5+1 if b // 5 != 0 else 1
+        ls.append([a, b])
+    ls.append([0, 1])
+    
+    ls = list(reversed(ls))
+
+    for i in range(n):
+        a, b = ls[i]
+        offset = ls[i-1][0] * 5 if i != 0 else 0
+        ret = ret[a - offset:b-offset]
+        ret = ret.replace("0", "00000")
+        ret = ret.replace("1", "11011")
+    a, b = ls[-1]
+    ret = ret[l-1-a:r-a]
+
+    return ret.count('1')
+
